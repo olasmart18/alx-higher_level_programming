26a2a93ddab091fadf73c729f102bbdfcfb8bd16
olasmartTech <eweleyeolamide10@gmail.com>
2023-03-14 11:07:53 -0700

    code error resolved

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,12 +1,13 @@
-class Rectangle {
+const Rectangle = class Rectangle {
     constructor(rec) {
-        // constructor body goes here
-    }
+        // constructor body goes here;
+    };
     r1() {
         console.log(this.rec);
-    }
-}
+    };
+};
 
 // const r1 = new Rectangle();
 // console.log(r1);
 // console.log(r1.constructor);
+module.exports = Rectangle;
