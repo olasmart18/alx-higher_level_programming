e58db6f8a792784f4bf230b0bdcb6a2d7be9710d
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-14 16:53:26 -0700

    task 2 completed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// .slice() method trims out the first 2
+// default arguments
+
+let args = process.argv.slice(2);
+
+// console.log(args.length);
+
+if (args.length > 1) {
+    console.log(' Arguments found');
+} else if (args.length === 1) {
+    console.log('Argument found');
+} else {
+    console.log('No argument');
+}
+
+
+
