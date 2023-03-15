98f6741c9402ff1720b8a6deaeb78da7a465aee8
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-15 00:27:16 -0700

    resolved code error

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -3,8 +3,4 @@ const Rectangle = require('./0-rectangle');
 
 const r1 = new Rectangle();
 console.log(r1);
-<<<<<<< HEAD
 console.log(r1.constructor);
-=======
-console.log(r1.constructor);
->>>>>>> 26a2a93ddab091fadf73c729f102bbdfcfb8bd16
--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,14 @@
 const Rectangle = class Rectangle {
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
-    };
-    r1() {
-        console.log(w, h);
-        console.log(this.w);
-        console.log(this.h);
-    }
-}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 
-module.exports = Rectangle;
+  r1 () {
+    console.log(this.width, this.height);
+    console.log(this.w);
+    console.log(this.h);
+  }
+};
+
+module.exports = Rectangle; /\\\\\n\g
