d13a7013df504c5f82c3f0f581f204110a5b8047
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-14 23:37:20 -0700

    task 2 done

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+const Rectangle = class Rectangle {
+    constructor(w, h){
+        this.width = w;
+        this.height = h;
+    };
+    r1(){
+        console.log(w, h);
+        console.log(this.w);
+        console.log(this.h);
+    }
+}
+
+module.exports = Rectangle;
