9da4098070fb8d46d403437dae73449af71ac82e
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-16 10:36:12 -0700

    task 4 resolved

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,14 +14,14 @@ const Rectangle = class Rectangle {
     }
   }
 
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-
   rotate () {
     this.width = this.height;
     this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
 module.exports = Rectangle;
