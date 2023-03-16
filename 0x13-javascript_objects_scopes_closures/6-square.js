63e7f77fe5d252433e9dbf74c0497597abce9e7a
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-16 13:38:23 -0700

    task 6 resolved

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/user/bin/node
+const Square_ = require('./5-square');
+
+const Square = class Square extends Square_ {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
+module.exports = Square;
