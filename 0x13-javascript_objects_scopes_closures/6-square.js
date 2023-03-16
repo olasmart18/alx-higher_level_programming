8af14bd6266bca47cf57920fbf54aef58561b3af
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-03-16 14:20:52 -0700

    resolved code output error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ const Square = class Square extends Square_ {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     } else {
       this.print();
